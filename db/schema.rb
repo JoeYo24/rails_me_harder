22a6959319f8bcdacb9853bcc6172b7d8147524a
JoeYo24 <jjsteffen@hotmail.com>
2023-06-21 18:06:09 -0400

    they work together now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_21_051737) do
+ActiveRecord::Schema.define(version: 2023_06_21_220127) do
 
   create_table "posts", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_06_21_051737) do
     t.datetime "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
